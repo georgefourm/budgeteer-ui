171bcbb6a872488a5eea1277ba561522bd791b09
George Fourmouzis <georgefourm@gmail.com>
2020-04-25 14:10:08 +0300

    Fix main routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.scss";
 import "antd/dist/antd.css";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Navigation from "./components/navigation/Navigation";
 
 import Dashboard from "./pages/dashboard";
 import Expenses from "./pages/expenses";
 import Incomes from "./pages/incomes";
 
-import { Layout, Menu } from "antd";
+import { Layout } from "antd";
 const { Header, Content } = Layout;
 
 function App() {
@@ -16,21 +17,14 @@ function App() {
       <Layout>
         <BrowserRouter>
           <Header>
-            <Menu theme="dark" mode="horizontal">
-              <Menu.Item key="/dashboard">
-                <Link to="/">Dashboard</Link>
-              </Menu.Item>
-              <Menu.Item key="/incomes">
-                <Link to="/incomes">Incomes</Link>
-              </Menu.Item>
-              <Menu.Item key="/expenses">
-                <Link to="/expenses/list">Expenses</Link>
-              </Menu.Item>
-            </Menu>
+            <Navigation />
           </Header>
           <Content className="content">
             <Switch>
               <Route exact path="/">
+                <Redirect to="/dashboard" />
+              </Route>
+              <Route exact path="/dashboard">
                 <Dashboard />
               </Route>
               <Route path="/incomes">
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+import { Menu } from "antd";
+
+export default function Navigation() {
+  const location = useLocation();
+  const [_, basePath] = location.pathname.split("/");
+
+  return (
+    <Menu theme="dark" mode="horizontal" selectedKeys={[basePath]}>
+      <Menu.Item key="dashboard">
+        <Link to="/dashboard">Dashboard</Link>
+      </Menu.Item>
+      <Menu.Item key="incomes">
+        <Link to="/incomes">Incomes</Link>
+      </Menu.Item>
+      <Menu.Item key="expenses">
+        <Link to="/expenses/list">Expenses</Link>
+      </Menu.Item>
+    </Menu>
+  );
+}
