aae98794ee15cbdb4f0e0efdcb57f8c72573a83d
George Fourmouzis <georgefourm@gmail.com>
2020-04-27 15:35:05 +0300

    Add dashboard totals

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,12 +1,56 @@
-import React from "react";
-import { Card } from "antd";
+import React, { useState, useEffect } from "react";
+import { Card, Col, Row, Statistic } from "antd";
+import { get } from "utils/network";
+import "./dashboard.scss";
 
 export default function Dashboard() {
+  const [stats, setStats] = useState({
+    totalExpenses: 0,
+    totalIncome: 0,
+  });
+  useEffect(() => {
+    get("dashboard").then((response) => {
+      setStats({
+        ...response,
+        balance: response.totalIncome - response.totalExpenses,
+      });
+    });
+  });
   return (
     <div>
-      <Card>
-        <p>Dashboard</p>
-      </Card>
+      <Row gutter={24}>
+        <Col span={8}>
+          <Card>
+            <Statistic
+              title="Total Income"
+              value={stats.totalIncome || 0}
+              precision={2}
+              suffix={<span>&euro;</span>}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card>
+            <Statistic
+              title="Total Expenses"
+              value={stats.totalExpenses || 0}
+              precision={2}
+              suffix={<span>&euro;</span>}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card>
+            <Statistic
+              title="Balance"
+              className={stats.balance > 0 ? "stat-positive" : "stat-negative"}
+              value={stats.balance}
+              precision={2}
+              suffix={<span>&euro;</span>}
+            />
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
