303103768fc826ba8633e2d26854984c46e78d40
George Fourmouzis <georgefourm@gmail.com>
2020-04-28 12:46:50 +0300

    Enhance dashboard balance panel

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,54 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { Card, Col, Row, Statistic } from "antd";
-import { get } from "utils/network";
+import React from "react";
+import { Card, Col, Row } from "antd";
 import "./dashboard.scss";
+import BalancePanel from "./BalancePanel";
 
 export default function Dashboard() {
-  const [stats, setStats] = useState({
-    totalExpenses: 0,
-    totalIncome: 0,
-  });
-  useEffect(() => {
-    get("dashboard").then((response) => {
-      setStats({
-        ...response,
-        balance: response.totalIncome - response.totalExpenses,
-      });
-    });
-  });
   return (
     <div>
+      <BalancePanel />
+      <br />
       <Row gutter={24}>
-        <Col span={8}>
-          <Card>
-            <Statistic
-              title="Total Income"
-              value={stats.totalIncome || 0}
-              precision={2}
-              suffix={<span>&euro;</span>}
-            />
-          </Card>
+        <Col span={16}>
+          <Card title="Expenses/Income by month" />
         </Col>
         <Col span={8}>
-          <Card>
-            <Statistic
-              title="Total Expenses"
-              value={stats.totalExpenses || 0}
-              precision={2}
-              suffix={<span>&euro;</span>}
-            />
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card>
-            <Statistic
-              title="Balance"
-              className={stats.balance > 0 ? "stat-positive" : "stat-negative"}
-              value={stats.balance}
-              precision={2}
-              suffix={<span>&euro;</span>}
-            />
-          </Card>
+          <Card title="Expenses breakdown by category" />
         </Col>
       </Row>
     </div>
